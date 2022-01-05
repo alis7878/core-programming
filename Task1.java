11a0fbc0f4d1a4b08c87e302be37fb97318c50d3
alis7878 <79696710+alis7878@users.noreply.github.com>
2022-01-05 10:04:01 +0530

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -9,6 +9,7 @@ public class Task1
         Scanner in = new Scanner(System.in);
         System.out.print("Enter Number of elements: ");
         int n = in.nextInt();
+        
         System.out.println("Enter the numbers");
         int[] arr=new int[100];
             for(int j=0;j < n-1;j++)
@@ -17,6 +18,8 @@ public class Task1
             }
             int MAX = in.nextInt();
             MAX = arr[0];
+        
+            //finding max value
             for(int i=0;i<n;i++)
             {
                 if(arr[i]>MAX)
