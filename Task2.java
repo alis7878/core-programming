b4d016d587747f1e0dd0a363748859789d9b8071
alis7878 <79696710+alis7878@users.noreply.github.com>
2022-01-05 10:02:35 +0530

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -1,36 +1,36 @@
 import java.util.*;
 import java.io.*;
 
-public class Task2
-{
+public class Task2 {
 
     public static void main(String args[]) {
 
-    String n;
-    String[] a;
-    int[] arr;
-    int index =0 ;
-    
-    Scanner in = new Scanner(System.in);
-    System.out.print("Enter Numbers with comma separated string: ");
-    n = in.nextLine();
+        String n;
+        String[] a;
+        int[] arr;
+        int index = 0;
 
-   a = n.split(",");
-    arr= new int[a.length]; 
-
-    for(int i=0;i<arr.length;i++){
-        arr[i] = Integer.parseInt(a[i]);
-    }
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Numbers with comma separated string: ");
+        //n -> string with coma separator
+        n = in.nextLine();
         
-    int l=arr[0];
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]>l)
-            {
-                 l = arr[i];      
-            }
-            
+        //a -> string without coma separator
+        a = n.split(",");
+        arr = new int[a.length];
+        
+        //parsing string into integer values
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(a[i]);
         }
-    System.out.println("Largest Number = " +l);
-}
+        
+        //finding max value
+        int MAX = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > MAX) {
+                MAX = arr[i];
+            }
+        }
+        System.out.println("Largest Number = " + MAX);
+    }
 }
