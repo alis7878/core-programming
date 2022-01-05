dff3d811740aae8e06a19b49208bfbd6131ac3ca
alis7878 <79696710+alis7878@users.noreply.github.com>
2022-01-05 09:50:59 +0530

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -4,26 +4,26 @@ import java.io.*;
 public class Task1
 {
 
-    public static void main(String args[]) {
-    Scanner in = new Scanner(System.in);
-    System.out.print("Enter Number of elements: ");
-    int n = in.nextInt();
-    System.out.println("Enter the numbers");
-    int[] arr=new int[100];
-        for(int j=0;j < n-1;j++)
-        {
-           arr[j] = in.nextInt();
-        }
-        int l = in.nextInt();
-        l = arr[0];
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]>l)
+    public static void main(String args[]) 
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Number of elements: ");
+        int n = in.nextInt();
+        System.out.println("Enter the numbers");
+        int[] arr=new int[100];
+            for(int j=0;j < n-1;j++)
             {
-                 l = arr[i];      
+                arr[j] = in.nextInt();
             }
-            
-        }
-    System.out.println("Largest Number = " +l);
-}
+            int MAX = in.nextInt();
+            MAX = arr[0];
+            for(int i=0;i<n;i++)
+            {
+                if(arr[i]>MAX)
+                {
+                    MAX = arr[i];      
+                }   
+            }
+        System.out.println("Largest Number = " +MAX);
+    }
 }
