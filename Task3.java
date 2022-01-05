1cfe83db6a7c202bf064fb8be4ea25e95b8fe16f
alis7878 <79696710+alis7878@users.noreply.github.com>
2022-01-05 10:12:23 +0530

    Update Task3.java

--- Task3.java
+++ Task3.java
@@ -1,36 +1,41 @@
 import java.util.*;
 import java.io.*;
 
-public class Task3
-{
-    
+public class Task3 {
+
     public static void main(String args[]) {
-        int k=0,d,f=0;
-    Scanner in = new Scanner(System.in);
-    System.out.print("Enter String: ");
-    String n = in.next();
-    System.out.println("Enter the no. of reverse pair");
-    int p = in.nextInt();
+        int k = 0, d, f = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter String: ");
+        String n = in.next();
+        
+        //p -> no. of alternate reverse
+        System.out.println("Enter the no. of reverse elements");
+        int p = in.nextInt();
 
-    char[] ch = new char[n.length()+50];
-  
-        for (int j = 0; j <n.length(); j++) {
-            ch[j] = n.charAt(j);}
+        char[] ch = new char[n.length()];
 
-            d=p-1;
-            f=d;
-     for( int i = 0; i<(n.length()/p); i++)
-    {
-        for(int j=0;j<p;j++)
-        {
-            ch[k]=n.charAt(d);
-            d--;
-            k++;
-            f++;
-        }d=f;
+        for (int j = 0; j < n.length(); j++) {
+            ch[j] = n.charAt(j);
+        }
+
+        d = p - 1;
+        f = d;
+        
+        //traversal 
+        for (int i = 0; i < (n.length() / p); i++) {
+            //reversing the characters
+            for (int j = 0; j < p; j++) {
+                ch[k] = n.charAt(d);
+                d--;
+                k++;
+                f++;
+            }
+            d = f;
+        }
+
+        for (char c : ch) {
+            System.out.print(c);
+        }
     }
-
-    for (char c : ch) {
-        System.out.print(c);
-    }
-}}
+}
